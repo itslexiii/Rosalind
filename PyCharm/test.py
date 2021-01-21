5ec61e047da7694bcb50e214e2af08ac53adafc6
Xinyue <xdeng@rockefeller.edu>
2021-01-21 23:11:20 +0800

    delete Spyder folder

--- PyCharm/test.py
+++
@@ -1,6 +0,0 @@
-print('Hello World!')
-for i in range(1, 100):
-    if i % 2 == 0:
-        print('Even!')
-    else:
-        print('Odd!')
--- Spyder/test1.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jan 21 16:45:03 2021
-
-@author: LEXi
-"""
-
-for i in range(10):
-    print(i)
