02dfa84c4d2730082b40f1e835786a10abe6be29
Xinyue <xdeng@rockefeller.edu>
2021-01-22 00:04:39 +0800

    finish count DNA nucleotides as sample question

--- PyCharm/Counting DNA Nucleotides.py
+++ PyCharm/Counting DNA Nucleotides.py
@@ -13,4 +13,22 @@ def openfile():
     return file
 
 
-print(openfile())
+def countDNA(string):
+    a = 0
+    g = 0
+    c = 0
+    t = 0
+    for i in string:
+        if i == 'A':
+            a += 1
+        elif i == 'G':
+            g += 1
+        elif i == 'C':
+            c += 1
+        elif i == 'T':
+            t += 1
+    return a, g, c, t
+
+
+st = openfile()
+print(countDNA(st))
