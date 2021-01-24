c079e65deb339b67d6d7f2d864eb370a5e8e6ee3
Xinyue <xdeng@rockefeller.edu>
2021-01-24 16:23:55 +0800

    Create Car class.

--- Basic Practice/Random Practice.py
+++ Basic Practice/Random Practice.py
@@ -78,3 +78,29 @@ while True:
 
 
 #%%
+
+# Create a Car class.
+
+class Car():
+    def __init__(self, make, model, color, year):
+        self.make = make
+        self.model = model
+        self.color = color
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model + ' ' + self.color
+        return long_name
+
+    def update_odometer(self, mileage):
+        self.odometer_reading = mileage
+
+    def read_odometer(self):
+        print('This car has ' + str(self.odometer_reading) + ' miles on it.')
+
+
+my_new_car = Car('Audi', 'A6', 'Black', 2018)
+my_new_car.update_odometer(200)
+print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
